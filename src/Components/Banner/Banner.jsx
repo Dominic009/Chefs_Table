6199bb8b2e0c304bb87f594834730556856e3b27
Dominic009 <dpg11000@gmail.com>
2024-03-16 01:40:35 +0600

    banner section compleeted

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,18 +2,18 @@ import BannerImg from '../../assets/Banner/Banner.png'
 const Banner = () => {
     return (
         <div className='relative'>
-            <div className='absolute text-center top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-[70%]'>
-                <h1 className='text-white text-6xl font-bold text-center leading-[60px] tracking-wide'>Discover an exciting way of eating healthy </h1>
-                <p className='text-white text-xl font-thin  mt-3'>You dont know how much you are missing out until you try it your self. With us, you can take a step without worrying about the any mistake.</p>
+            <div className='absolute text-center top-[60%] md:top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[70%]'>
+                <h1 className='text-white text-xl md:text-6xl font-bold text-center md:leading-[60px] tracking-wide'>Discover an exciting way of eating healthy </h1>
+                <p className='text-white text-xs md:text-xl font-thin md:mt-3'>You dont know how much you are missing out until you try it your self. <span className='hidden md:block'> With us, you can take a step without worrying about the any mistake.</span></p>
 
-                <div className='flex gap-7 justify-center mt-7'>
+                <div className='flex gap-7 justify-center md:mt-7 scale-[50%] md:scale-100'>
                     <button className='btn rounded-full px-5 text-lg bg-[#0be58a] border-none hover:bg-white hover:text-[#068f56]'>Explore now</button>
                     <button className='btn rounded-full px-5 text-lg bg-transparent text-white hover:bg-[#0be58a]'>Feedbacks</button>
                 </div>
             </div>
             
-            <div>
-                <img src={BannerImg} />
+            <div className='md:w-full '>
+                <img className='shadow-2xl rounded-2xl' src={BannerImg} />
             </div>
 
         </div>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,13 +3,13 @@ import { IoIosSearch } from "react-icons/io";
 import { FaRegUserCircle } from "react-icons/fa";
 const Header = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-center">
+    <div className="grid grid-cols-2 md:grid-cols-3 items-center gap-3">
       <div>
         <img src={Logo} className="w-52"/>
       </div>
 
-      <div>
-        <ul className="flex gap-8 text-gray-300 cursor-pointer -mr-[35px]">
+      <div className="hidden md:block">
+        <ul className="flex justify-center gap-8 text-gray-300 cursor-pointer">
           <li className="hover:text-white hover:scale-110">Home</li>
           <li className="hover:text-white hover:scale-110">Recipes</li>
           <li className="hover:text-white hover:scale-110">About</li>
@@ -17,13 +17,13 @@ const Header = () => {
         </ul>
       </div>
 
-      <div className="flex items-center gap-3">
+      <div className="flex justify-center gap-1 items-center scale-[50%] md:scale-100">
         <div className="flex items-center relative">
-          <input className="px-7 py-1 rounded-2xl outline-none opacity-85 hover:opacity-100" type= "text" placeholder="Search" />
-          <IoIosSearch className="absolute left-2 scale-110 text-gray-500" />
+          <input className="px-3 md:px-7 py-1 rounded-2xl outline-none opacity-85 hover:opacity-100" type= "text" placeholder="Search" />
+          <IoIosSearch className="absolute left-[33%] md:left-2 scale-110 text-gray-500" />
         </div>
 
-        <div className="bg-white rounded-full p-3 cursor-pointer">
+        <div className="bg-white rounded-full p-3 cursor-pointer scale-95 md:scale-100">
           <FaRegUserCircle className="scale-150 text-[#068f56]"></FaRegUserCircle>
         </div>
       </div>
