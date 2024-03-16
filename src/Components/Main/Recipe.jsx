10923ec3596c67d6eef4eedbbdb83e0bc3657ebe
Dominic009 <dpg11000@gmail.com>
2024-03-16 13:34:52 +0600

    cooking and preparing section work

--- src/Components/Main/Recipe.jsx
+++ src/Components/Main/Recipe.jsx
@@ -28,7 +28,46 @@ const Recipe = () => {
 
         {/* right side */}
         <div className="w-[40%] border border-red-500">
-          <h1>Cooking section</h1>
+          <h1>Ready to cook : 0</h1>
+          <hr />
+          <div>
+          <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      {/* row 2 */}
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+      {/* row 3 */}
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+          </div>
         </div>
       </div>
     </div>
