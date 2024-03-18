1d27a28c01fbbb6348720be20045975492a9a1c4
Dominic009 <dpg11000@gmail.com>
2024-03-18 12:12:21 +0600

    Deploying site

--- src/Components/Main/Cooking.jsx
+++ src/Components/Main/Cooking.jsx
@@ -1,24 +1,24 @@
-const Cooking = () => {
+const Cooking = ({dish, index}) => {
 
+  const {recipe_name, preparing_time, calories} = dish;
 
   return (
     <div>
-      <div className="flex text-gray-500 hover:bg-green-300 hover:text-green-900 hover:font-semibold fira">
-        <td className="flex justify-center items-center text-gray-600">
-          {index + 1}
+      <div className="flex text-gray-500 hover:bg-green-300 hover:text-green-900 hover:font-semibold fira items-center justify-between">
+        <td className="flex justify-center items-center text-gray-600">{index + 1}
         </td>
-        <td>{que}</td>
-        <td>{que} </td>
-        <td>{que}</td>
+        
+        <td>{recipe_name}</td>
+        <td>{preparing_time}</td>
+        <td>{calories}</td>
 
-        <div className="mt-2 flex">
-          <button
-            className="btn bg-green-500 hover:bg-white hover:text-green-800 text-white scale-90 rounded-full"
-          >
-            Prepare
-          </button>
+        <div className=" bg-green-500 text-white rounded px-3 h-5 mr-2">
+
+           Cooking
+
         </div>
       </div>
+      <hr className=" border-b border-b-gray-200 w-[80%] mx-auto" />
     </div>
   );
 };
--- src/Components/Main/Recipe.jsx
+++ src/Components/Main/Recipe.jsx
@@ -3,12 +3,14 @@ import Items from "./Recipe items/Items";
 import Que from "./Que";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Cooking from './Cooking'
 
 
 
 const Recipe = () => {
   const [recipes, setRecipes] = useState([]);
   const [cook, setCook] = useState([]);
+  const [currentList, setCurrentList] = useState([])
 
   useEffect(() => {
     fetch("./recipes.json")
@@ -22,6 +24,7 @@ const Recipe = () => {
     const isExist = cook.find((res) => res.recipe_id == item.recipe_id);
     if (!isExist) {
       setCook([...cook, item]);
+      
     } else {
       notify();
     }
@@ -29,7 +32,14 @@ const Recipe = () => {
 
   const handlePrepare = (id) => {
     const updatedCook = cook.filter((item) => item.recipe_id !== id);
-    setCook(updatedCook);
+    // const updateCurentList = currentList.find((item) => item.id !== id);
+    if(updatedCook){
+      setCook(updatedCook);
+      setCurrentList([...currentList, updatedCook])
+    }
+    else{
+      
+    }
   }
 
   return (
@@ -65,7 +75,7 @@ const Recipe = () => {
                 <table className="table text-gray-500">
                   {/* head */}
                   <thead className="flex">
-                    <tr className="flex m:justify-end md:gap-2 w-[80%] text-[16px]">
+                    <tr className="flex md:justify-end md:gap-2 w-[80%] text-[16px]">
                       <th>Name</th>
                       <th>Time</th>
                       <th>Calories</th>
@@ -90,7 +100,7 @@ const Recipe = () => {
           {/* Cooking que */}
           <div className="mt-7 border rounded-xl p-3">
             <div>
-              <h1 className="text-2xl font-bold text-white mb-2">Cooking que : 0</h1>
+              <h1 className="text-2xl font-bold text-white mb-2">Cooking que : {currentList.length}</h1>
             </div>
             <div>
               <div className="overflow-x-auto bg-white md:p-5 rounded-xl">
@@ -105,9 +115,11 @@ const Recipe = () => {
                   </thead>
                   <tbody className="flex">
                     {/* row 1 */}
-                    <tr className=" bg-gray-100 border border-green-400">
+                    <tr className=" bg-gray-100 border border-green-400 w-full">
                       {/* <th>1</th> */}
-
+                        {
+                          currentList.map((dish, index) => <Cooking key={index} dish={dish} index={index}></Cooking>)
+                        }
                     </tr>
                   </tbody>
                 </table>
