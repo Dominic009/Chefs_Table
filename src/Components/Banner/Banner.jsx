515c8d9b54fd597e6c3b18a81d57f39133f82b73
Dominic009 <dpg11000@gmail.com>
2024-04-24 15:07:42 +0600

    slight changes

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,8 +1,10 @@
 import BannerImg from '../../assets/Banner/Banner.png'
+
+
 const Banner = () => {
     return (
         <div className='relative'>
-            <div className='absolute text-center top-[60%] md:top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[70%]'>
+            <div className='absolute text-center top-[60%] md:top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 lg:w-[70%] md:w-[100%]'>
                 <h1 className='text-white text-xl md:text-6xl font-bold text-center md:leading-[60px] tracking-wide'>Discover an exciting way of eating healthy </h1>
                 <p className='text-white text-xs md:text-xl font-thin md:mt-3'>You dont know how much you are missing out until you try it your self. <span className='hidden md:block'> With us, you can take a step without worrying about the any mistake.</span></p>
 
